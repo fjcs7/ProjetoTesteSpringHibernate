0da46bcb7f5dc3e63301633fad1d4bfac14bd8e0
fernando.sousa <fernando.sousa@PC-1679.lggo.com.br>
2017-06-08 14:13:27 -0300

    ApplicationContextConfig agora obtém as classes anotadas pelo entity do hibernate

--- src/main/java/net/codejava/spring/config/ApplicationContextConfig.java
+++ src/main/java/net/codejava/spring/config/ApplicationContextConfig.java
@@ -1,25 +1,29 @@
 package net.codejava.spring.config;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import net.codejava.spring.dao.RedeSocialDAO;
-import net.codejava.spring.dao.RedeSocialDAOI;
-import net.codejava.spring.dao.UserDAO;
-import net.codejava.spring.dao.UserDAOImpl;
-import net.codejava.spring.model.User;
-
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
+import org.hibernate.metamodel.domain.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import net.codejava.spring.dao.RedeSocialDAO;
+import net.codejava.spring.dao.RedeSocialDAOI;
 
 @Configuration
 @ComponentScan("net.codejava.spring")
@@ -53,13 +57,38 @@ public class ApplicationContextConfig {
     	properties.put("hibernate.hbm2ddl.auto","update");
     	return properties;
     }
+
+    private Class[] getClassesAnotadas(){
+    	  ArrayList<Class> classes = new ArrayList<Class>();
+
+    	  // the following will detect all classes that are annotated as @Entity
+    	  ClassPathScanningCandidateComponentProvider scanner =
+    	    new ClassPathScanningCandidateComponentProvider(false);
+    	  scanner.addIncludeFilter(new AnnotationTypeFilter(javax.persistence.Entity.class));
+
+    	  // only register classes within "com.fooPackage" package
+    	  for (BeanDefinition bd : scanner.findCandidateComponents("net.codejava.spring.model")) {
+    	    String name = bd.getBeanClassName();
+    	    try {
+    	      classes.add(Class.forName(name));
+    	    } catch (Exception E) {
+    	      // TODO: handle exception - couldn't load class in question
+    	    }
+    	  } // for
+
+    	  return classes.toArray(new Class[classes.size()]);
+    }
     
     @Autowired
     @Bean(name = "sessionFactory")
     public SessionFactory getSessionFactory(DataSource dataSource) {
+    	
+    	AnnotationSessionFactoryBean anotationSeesionFactoryBean = new AnnotationSessionFactoryBean();
+    	anotationSeesionFactoryBean.setDataSource(dataSource);
+    	anotationSeesionFactoryBean.setPackagesToScan("net.codejava.spring.model");
     	LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
     	sessionBuilder.addProperties(getHibernateProperties());
-    	sessionBuilder.addAnnotatedClasses(User.class);
+    	sessionBuilder.addAnnotatedClasses(getClassesAnotadas());
     	return sessionBuilder.buildSessionFactory();
     }
     
@@ -69,12 +98,6 @@ public class ApplicationContextConfig {
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
 		return transactionManager;
 	}
-    
-    @Autowired
-    @Bean(name = "userDao")
-    public UserDAO getUserDao(SessionFactory sessionFactory) {
-    	return new UserDAOImpl(sessionFactory);
-    }
     
     @Autowired
     @Bean(name = "redeSocialDao")
